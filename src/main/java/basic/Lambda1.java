8ac53e6b4473391a9ee547250c25326da065a407
Jiajie <jiajie.ma@thoughtworks.com>
2021-07-07 17:03:25 +0800

    add lambda1 which listen sns

--- src/main/java/basic/Lambda1.java
+++ src/main/java/basic/Lambda1.java
@@ -1,4 +1,20 @@
 package basic;
 
-public class Lambda1 {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+
+public class Lambda1 implements RequestHandler<SNSEvent, String> {
+    @Override
+    public String handleRequest(SNSEvent event, Context context) {
+        //print log to cloud watch
+        LambdaLogger logger = context.getLogger();
+        StringBuilder response = new StringBuilder("lambda1 200 OK");
+        for (SNSEvent.SNSRecord msg : event.getRecords()) {
+            response.append(msg);
+        }
+        logger.log(response.toString());
+        return response.toString();
+    }
 }
